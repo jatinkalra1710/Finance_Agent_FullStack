ef3ded1b83db3b5a12d37211d3d7d2b1da9c59be
Jatin Kalra <142911658+jatinkalra1710@users.noreply.github.com>
2026-04-21 14:47:02 +0530

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth"; // <-- THIS LINE WAS MISSING!
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
